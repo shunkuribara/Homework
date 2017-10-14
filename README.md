4b5a1cb8278ed001199e2da28b5de194b57d4285
shunkuribara <32214582+shunkuribara@users.noreply.github.com>
2017-10-14 15:54:26 +0900

    twitterAPI
    twitterAPIの編集

--- README.md
+++ README.md
@@ -2,3 +2,4 @@
 pythonの課題
 
 hello world!!
+print('hello world')
---
+++ xxx.py
@@ -0,0 +1,23 @@
+from requests_oauthlib import OAuth1Session
+import json
+import os
+
+CONSUMER_KEY = os.environ['TWITTER_CONSUMER_KEY']
+CONSUMER_SECRET = os.environ['TWITTER_CONSUMER_SECRET']
+ACCESS_TOKEN = os.environ['TWITTER_ACCESS_TOKEN']
+ACCESS_TOKEN_SECRET = os.environ['TWITTER_ACCESS_SECRET']
+
+
+twitter = OAuth1Session(CONSUMER_KEY, CONSUMER_SECRET, ACCESS_TOKEN, ACCESS_TOKEN_SECRET)
+params = {}
+#タイムラインを取得
+req = twitter.get("https://api.twitter.com/1.1/statuses/home_timeline.json", params = params)
+timeline = json.loads(req.text)
+for tweet in timeline:
+  print(tweet['text'])
+  print(tweet['favorite_count'])
+
+#ツイートする
+params = {"status": "Hello, World!"}
+req = twitter.post("https://api.twitter.com/1.1/statuses/update.json",params = params)
+
